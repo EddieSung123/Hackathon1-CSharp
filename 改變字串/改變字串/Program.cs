4df621f2d0579054d290a14f6009c1e1452b5f04
宋浩茹 <eerica841205@gmail.com>
2018-05-24 22:04:13 +0800

    add file

---
+++ 改變字串/改變字串/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 改變字串
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(var i = 1; i<101; i++)
+            {
+                string input = i.ToString();
+                input = input.Replace('3', 'A').Replace('5', 'B').Replace('9', 'C').Replace('0', 'D');
+
+                Console.WriteLine(input);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
