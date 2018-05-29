50bb13bc081cb79271edb3764c6d9fb9447cba66
宋浩茹 <eerica841205@gmail.com>
2018-05-29 22:15:50 +0800

    guess

---
+++ 糖果/糖果/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 糖果
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int candy =0;
+            int candyp=0;
+            Console.Write("你想要吃的糖果數 : ");
+            string input = Console.ReadLine();
+            var number = int.Parse(input);
+
+
+            while (number >= 0)
+            {
+                candyp++;
+                if(candyp == 3)
+                {
+                    candyp = 0;                    
+                }
+                else
+                {
+                    candy++;
+                }
+                number--;
+            }
+
+            Console.WriteLine("需要買 : " + candy + "個糖果");
+            Console.ReadLine();
+
+        }
+    }
+}
